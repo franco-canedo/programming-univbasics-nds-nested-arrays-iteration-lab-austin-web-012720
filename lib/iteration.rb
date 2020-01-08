5e4b94cc13c7a67af899ca980e709dfc40b4a791
Franco Canedo <francanedo1@hotmail.com>
2020-01-08 20:30:46 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -12,15 +12,9 @@ def join_ingredients(src)
   while row_index < src.count do 
       new_array << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
       row_index += 1
-    
-    
   end 
   
   new_array
-  
-  
-  
-  
 end
 
 def find_greater_pair(src)
