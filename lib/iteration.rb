f11dd448a98f27b7e9f0b20c73957e8324680f73
Franco Canedo <francanedo1@hotmail.com>
2020-01-08 20:27:20 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -13,9 +13,9 @@ def join_ingredients(src)
     new_array = []
     while element_index < src[row_index].count do 
       new_array << "I love #{src[row_index][0]} and #{src[row_index][1]} on py pizza"
-      element_index += 1 
+      row_index += 1
     end
-    row_index += 1 
+    
   end 
   
   
