e75eb27fb846d6d2437c1a2dec185292bd0c3aa3
Franco Canedo <francanedo1@hotmail.com>
2020-01-08 20:39:04 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -49,6 +49,7 @@ def total_even_pairs(src)
   while row_index < src.count do 
     if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
       total += src[row_index][0]
+      total += src[row_index][1]
     end 
   end 
   
